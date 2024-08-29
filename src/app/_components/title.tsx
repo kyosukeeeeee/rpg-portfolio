40f28e3dd931de7ef4069cadd19d205da7ec1774
kyosukeeeeee <mikeneko3511@gmail.com>
2024-08-29 19:21:03 +0900

    タイトル画面の追加

--- src/app/_components/title.tsx
+++ src/app/_components/title.tsx
@@ -1,6 +1,13 @@
-const Title = () => {
-    return (
+import "../title.scss"
 
+const Title: React.FC<{ text: string }> = ({text}) => {
+    return (
+        <div className="bg">
+            <h1 className="title">Port Folio.</h1>
+            <a className="button" href="/home">
+                <p>{text} to Start.</p>
+            </a>
+        </div>
     )
 }
 
